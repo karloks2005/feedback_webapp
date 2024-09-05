7be71a8bb03617eb5d93d57b988c203110db10e4
karloks2005 <karloks2005@gmail.com>
2024-09-05 22:59:27 +0200

    Final

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,31 +1,28 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext(); 
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This is a sample feedback',
-            rating: 5
-        },
-        {
-            id: 2,
-            text: 'This is a sample feedback2',
-            rating: 1
-        },
-        {
-            id: 3,
-            text: 'This is a sample feedback3',
-            rating: 3
-        }
-    ])
-
+    const [isLoading, setIsLoading] = useState(true);
+    const [feedback, setFeedback] = useState([])
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     });
+
+    useEffect(() => {
+        fetchFeedback();
+    }, [])
+
+    //Fetch feedback
+    const fetchFeedback = async () => {
+        const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc');
+
+        const data = await response.json();
+        setFeedback(data);
+        setIsLoading(false);
+    }
 
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
@@ -59,6 +56,7 @@ export const FeedbackProvider = ({ children }) => {
         editFeedback,
         feedbackEdit,
         updateFeedback,
+        isLoading,
     }}>
         {children}
     </FeedbackContext.Provider>
